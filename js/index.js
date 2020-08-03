663cad4404e8a498b9dcad19faf0293b23f9728b
Cedric Damian <cedricdamian19@gmail.com>
2020-08-04 00:02:49 +0300

    Add Background on 'About' page

---
+++ js/index.js
@@ -0,0 +1,18 @@
+/*import React from 'react';
+import 'css/index.css';
+import cover from 'images/pizza2.jpg';
+
+function About() {
+  return(
+    <div className="about" style={{backgroundImage: "url(${cover})"}}>
+      
+      <div className="about-content">
+        
+      </div>
+
+    </div>
+  );
+};
+
+const domContainer = document.querySelector(".about");
+ReactDOM.render(e(About), domContainer);*/
