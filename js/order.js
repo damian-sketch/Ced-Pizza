0eba2d22bc2474fbd5ca718966a60592df585396
Cedric Damian <cedricdamian19@gmail.com>
2020-08-06 01:25:06 +0300

    Improve functionality on the order calculator

--- js/order.js
+++ js/order.js
@@ -1,10 +1,14 @@
 function totalPrice(){
-  var size = parseInt(document.getElementById("size").value);
-  var crusts = parseInt(document.getElementById("crusts").value);
-  var toppings = parseInt(document.getElementById("toppings").value);
-  var price = 0;
 
-  price = size + crusts + toppings;
+  if ((document.getElementById("size").checked) && (document.getElementById("crusts").checked) && (document.getElementById("toppings").checked)){
+    var size = parseInt(document.getElementById("size").value);
+    var crusts = parseInt(document.getElementById("crusts").value);
+    var toppings = parseInt(document.getElementById("toppings").value);
+    var price = 0;
 
-  alert("Your order costs " + price);
+    price = size + crusts + toppings;
+
+    alert("Your order costs " + price);
+  }
+  
 };
